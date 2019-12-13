199805f9c394f5fe95c862c1deccc64a7ce377f7
lyeeedar <lyeeedar@gmail.com>
2019-12-13 11:47:07 +0000

    First commit of project

---
+++ GitApp/Util/ProcessUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitApp
+{
+	public class ProcessUtils
+	{
+		public static void ExecuteCmd(string cmd, string workingDirectory, Action<string> processOutput, Action<string> processError)
+		{
+			using (Process p = new Process())
+			{
+				p.StartInfo = new ProcessStartInfo()
+				{
+					FileName = @"C:\Windows\System32\cmd.exe",
+					CreateNoWindow = true,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					UseShellExecute = false,
+					WorkingDirectory = workingDirectory,
+					Arguments = "/c " + cmd
+				};
+				p.OutputDataReceived += (sender, args) =>
+				{
+					if (args.Data != null) processOutput(args.Data);
+				};
+				p.ErrorDataReceived += (sender, args) =>
+				{
+					if (args.Data != null) processError(args.Data);
+				};
+
+				p.EnableRaisingEvents = true;
+
+				p.Start();
+
+				p.BeginOutputReadLine();
+				p.BeginErrorReadLine();
+
+				bool exited = false;
+				Task.Run(() => 
+				{
+					Thread.Sleep(5000);
+
+					if (!exited)
+					{
+						try
+						{
+							p.Kill();
+							processError("Force kill");
+						}
+						catch (Exception) { }
+					}
+				});
+				p.WaitForExit();
+				exited = true;
+			}
+		}
+	}
+}
---
+++ GitApp/ViewModel.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Xml.Serialization;
+using WPFFolderBrowser;
+
+namespace GitApp
+{
+	public class ViewModel : NotifyPropertyChanged
+	{
+		//-----------------------------------------------------------------------
+		public string ProjectName
+		{
+			get { return m_projectName; }
+			set
+			{
+				m_projectName = value;
+				RaisePropertyChangedEvent();
+			}
+		}
+		private string m_projectName;
+
+		//-----------------------------------------------------------------------
+		public string Branch
+		{
+			get { return m_branch; }
+			set
+			{
+				m_branch = value;
+				RaisePropertyChangedEvent();
+			}
+		}
+		private string m_branch;
+
+		//-----------------------------------------------------------------------
+		public int NumberCommitsToPull
+		{
+			get { return m_numberCommitsToPull; }
+			set
+			{
+				m_numberCommitsToPull = value;
+				RaisePropertyChangedEvent();
+			}
+		}
+		private int m_numberCommitsToPull;
+
+		//-----------------------------------------------------------------------
+		public bool NotARepo
+		{
+			get { return m_notARepo; }
+			set
+			{
+				if (m_notARepo != value)
+				{
+					m_notARepo = value;
+					RaisePropertyChangedEvent();
+				}
+			}
+		}
+		private bool m_notARepo;
+
+		//-----------------------------------------------------------------------
+		public string CurrentDirectory
+		{
+			get { return m_currentDirectory; }
+			set
+			{
+				m_currentDirectory = value;
+				RaisePropertyChangedEvent();
+
+				ProjectName = Path.GetFileName(CurrentDirectory);
+
+				CheckStatus();
+			}
+		}
+		private string m_currentDirectory;
+
+		//-----------------------------------------------------------------------
+		public string SettingsPath = Path.GetFullPath("Settings.xml");
+		public SerializableDictionary<string, string> Settings { get; set; }
+
+		//-----------------------------------------------------------------------
+		public Command<object> ChangeDirectoryCMD { get { return new Command<object>((obj) => { ChangeDirectory(); }); } }
+
+		//-----------------------------------------------------------------------
+		public ViewModel()
+		{
+			if (File.Exists(SettingsPath))
+			{
+				using (var filestream = File.Open(SettingsPath, FileMode.Open, FileAccess.Read))
+				{
+					try
+					{
+						var serializer = new XmlSerializer(typeof(SerializableDictionary<string, string>));
+						Settings = (SerializableDictionary<string, string>)serializer.Deserialize(filestream);
+					}
+					catch (Exception ex)
+					{
+						Message.Show(ex.Message + ex.ToString(), "Exception!");
+						Settings = new SerializableDictionary<string, string>();
+					}
+				}
+			}
+			else
+			{
+				Settings = new SerializableDictionary<string, string>();
+			}
+
+			CurrentDirectory = GetSetting<string>("CurrentDirectory");
+
+			var timer = new Timer();
+			timer.Elapsed += (e, args) =>
+			{
+				CheckStatus();
+			};
+			timer.Interval = 3000;
+			timer.Start();
+		}
+
+		//-----------------------------------------------------------------------
+		public void ChangeDirectory()
+		{
+			var dlgRoot = new WPFFolderBrowserDialog();
+			dlgRoot.ShowPlacesList = true;
+			dlgRoot.Title = "Git Project";
+			bool? resultRoot = dlgRoot.ShowDialog();
+
+			if (resultRoot == true)
+			{
+				ProcessUtils.ExecuteCmd(
+					"git rev-parse --show-toplevel", 
+					dlgRoot.FileName, 
+					(output) => 
+					{
+						CurrentDirectory = output;
+						StoreSetting("CurrentDirectory", CurrentDirectory);
+					}, 
+					(error) => 
+					{
+						Message.Show("The selected directory is no a valid git project", "Invalid Directory");
+					});
+			}
+		}
+
+		//-----------------------------------------------------------------------
+		public void CheckStatus()
+		{
+			ProcessUtils.ExecuteCmd(
+				"git status",
+				CurrentDirectory,
+				(output) =>
+				{
+					NotARepo = false;
+
+					if (output.StartsWith("On branch"))
+					{
+						Branch = output.Replace("On branch", "").Trim();
+					}
+					else if (output.StartsWith("Your branch is behind"))
+					{
+						var split = output.Split(new string[] { " by ", " commits" }, StringSplitOptions.RemoveEmptyEntries);
+						var countStr = split[1];
+						var count = int.Parse(countStr);
+
+						NumberCommitsToPull = count;
+					}
+				},
+				(error) =>
+				{
+					if (error.StartsWith("fatal: Not a git repository (or any of the parent directories)") || error.StartsWith("Force kill"))
+					{
+						NotARepo = true;
+						return;
+					}
+
+					NotARepo = false;
+				});
+		}
+
+		//-----------------------------------------------------------------------
+		public void StoreSetting(string key, object value)
+		{
+			string valueAsString = value.SerializeObject();
+
+			Settings[key] = valueAsString;
+			Directory.CreateDirectory(System.IO.Path.GetDirectoryName(SettingsPath));
+
+			if (!File.Exists(SettingsPath)) File.Create(SettingsPath).Close();
+
+			using (var filestream = File.Open(SettingsPath, FileMode.Truncate, FileAccess.Write))
+			{
+				try
+				{
+					var serializer = new XmlSerializer(Settings.GetType());
+					serializer.Serialize(filestream, Settings);
+				}
+				catch (Exception e)
+				{
+					Message.Show(e.Message, "Exception!");
+				}
+			}
+		}
+
+		//-----------------------------------------------------------------------
+		public T GetSetting<T>(string key, T fallback = default(T))
+		{
+			if (Settings.ContainsKey(key)) return Settings[key].DeserializeObject<T>();
+			else return fallback;
+		}
+	}
+}
